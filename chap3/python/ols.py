7a6c32c009bcebc74fd714cc3e49c03e23ba004e
axect <axect@outlook.kr>
2021-04-26 16:25:38 +0900

    t-test for Python

--- chap3/python/ols.py
+++ chap3/python/ols.py
@@ -1,39 +1,94 @@
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import scipy.stats as ss
 
-# Prepare Data to Plot
-x = np.arange(1, 5, 0.1)
-err = np.random.randn(len(x))
-y = 2 * x + 3 + err
+# ==============================================================================
+# Main
+# ==============================================================================
+def main():
+    np.random.seed(42)
 
-# X = (1 | x)
-X = np.matrix(x).T      # To Column matrix
-X = sm.add_constant(X)  # Add constant
-Y = np.matrix(y).T      # To Column matrix
+    # Prepare Data to Plot
+    x = np.arange(1, 5, 0.1)
+    err = np.random.randn(len(x))
+    y = 2 * x + 3 + err
 
+    ols = OLSEstimator(x, y)
+    ols.estimate()
+    ols.test()
+
+    print("beta:", ols.beta_hat)
+    print("t_score:", ols.t_score)
+    print("p_value:", ols.p_value)
+
+    with plt.xkcd():
+        # Prepare Plot
+        plt.figure(figsize=(10,6), dpi=300)
+        plt.title(r"Simple OLS", fontsize=16)
+        plt.xlabel(r'x', fontsize=14)
+        plt.ylabel(r'y', fontsize=14)
+
+        # Plot with Legends
+        plt.scatter(x, y, label='Data')
+        plt.plot(x, np.asarray(ols.y_hat).ravel(), 'r', label='fit')
+
+        # Other options
+        plt.legend(fontsize=12)
+
+    plt.savefig("simple_ols.png", dpi=300)
+
+
+# ==============================================================================
+# Estimate
+# ==============================================================================
 def find_beta_hat(X, y): # X should be np.matrix
     return np.linalg.pinv(X) * y
 
-def find_y_hat(X, y):
-    beta_hat = find_beta_hat(X, y)
-    return X * beta_hat
+def find_y_hat(X, beta, y):
+    return X * beta
 
-Y_hat = find_y_hat(X, Y)
+# ==============================================================================
+# Test
+# ==============================================================================
+def find_sigma_hat(y, y_hat, p):
+    return np.sum((y - y_hat)**2) / (len(y) - p - 1)
 
-with plt.xkcd():
-    # Prepare Plot
-    plt.figure(figsize=(10,6), dpi=300)
-    plt.title(r"Simple OLS", fontsize=16)
-    plt.xlabel(r'x', fontsize=14)
-    plt.ylabel(r'y', fontsize=14)
+def calc_t_score(beta, X, sigma):
+    v = np.sqrt(np.diag(np.linalg.inv(X.T * X)))
+    return (beta / v) / np.sqrt(sigma)
+
+def calc_p_value(d, z):
+    return (1 - d.cdf(z)) * 2
+
+# ==============================================================================
+# OOP Implementation
+# ==============================================================================
+class OLSEstimator:
+    def __init__(self, x, y):
+        X = np.matrix(x).T
+        X = sm.add_constant(X)
+        Y = np.matrix(y).T
+        self.X = X
+        self.Y = Y
     
-    # Plot with Legends
-    plt.scatter(x, y, label='Data')
-    plt.plot(x, np.squeeze(np.asarray(Y_hat)), 'r', label='fit')
+    def estimate(self):
+        self.beta_hat = find_beta_hat(self.X, self.Y)
+        self.y_hat = find_y_hat(self.X, self.beta_hat, self.Y)
     
-    # Other options
-    plt.legend(fontsize=12)
+    def test(self):
+        N = self.Y.shape[0]
+        p = self.X.shape[1]-1
 
-plt.savefig("simple_ols.png", dpi=300)
+        self.sigma_hat = find_sigma_hat(np.asarray(self.Y).ravel(), np.asarray(self.y_hat).ravel(), self.X.shape[1]-1)
+        self.t_score = calc_t_score(np.asarray(self.beta_hat).ravel(), self.X, self.sigma_hat)
+        
+        t_dist = ss.t(df=N-p-1)
 
+        p_fun = lambda t: calc_p_value(t_dist, t)
+        p_fun = np.vectorize(p_fun)
+
+        self.p_value = p_fun(self.t_score)
+
+if __name__ == "__main__":
+    main()
